8f0e83f5c8a5eb9370c44f3a1a998f407dd540c5
Vidya Sambasivam <vidyasambas@gmail.com>
2015-07-23 21:45:25 +0530

    Revert "Comments included. better indentation done"
    This reverts commit 67d863aa0bbda7b85854e673582a19e271847bae.

--- cachematrix.R
+++ cachematrix.R
@@ -4,33 +4,12 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  inv_m <- NULL
-  set <- function(y) {
-    x <<- y
-    inv_m <<- NULL
-  }
-  get <- function() x
-  setinv <- function(im1) inv_m <<- im1
-  getinv <- function() inv_m
-  list(set = set, get = get,
-       setinv = setinv,
-       getinv = getinv)
+
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
-  
-  inv_m <- x$getinv()
-  if(!is.null(inv_m)) {
-    message("getting cached data")
-    return(inv_m)
-  }
-  data_m <- x$get()
-  "ma"
-  inv_m <- solve(data_m)
-  x$setinv(inv_m)
-  inv_m
+        ## Return a matrix that is the inverse of 'x'
 }
